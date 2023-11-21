018324819b01299ccc949448339e47a2eb8629cb
Malvin A <malvinaudriansyah77@gmail.com>
2023-11-21 17:20:56 +0700

    main file tweak

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
