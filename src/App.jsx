8ed54703dc023938fefad67b5d5ca96c32651a4d
Malvin A <malvinaudriansyah77@gmail.com>
2023-11-27 12:29:04 +0700

    Significant Update (Chakra UI Implementation)
    - Installed Chakra UI into the project
    - Mobile and Tablet responsibility
    - Mobile Layout
    - Width tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,18 @@ import jsLogo from "/js.png";
 import project1 from "/project1.png";
 
 import "./style.css";
+import {
+  Box,
+  Center,
+  Container,
+  Divider,
+  Grid,
+  GridItem,
+  HStack,
+  Heading,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 
 //Components
 
@@ -28,49 +40,142 @@ function Navbar(props) {
           <span>Blog</span>
         </div>
       </div>
-      <hr style={{ maxWidth: "1140px", margin: "auto" }} />
+      <Center>
+        <Divider height={"1px"} maxWidth={"1140px"}></Divider>
+      </Center>
     </div>
   );
 }
 
-function Intro(props) {
+function Intro() {
   return (
-    <div className="intro">
-      <h2>{props.intro}</h2>
-      <img className="photo" src={displayPic} alt="" />
-      <div className="bio">
-        <h3>Biography</h3>
-        <p>
+    <Grid
+      templateColumns="200px 1fr 250px"
+      templateRows="auto"
+      gap={6}
+      mt={"48px"}
+    >
+      <GridItem display={[null, null, "none"]} colSpan={3}>
+        <Box
+          margin={"auto"}
+          display={"flex"}
+          borderRadius={"full"}
+          w={"170px"}
+          h={"170px"}
+          bgGradient={`linear(to-r, rgba(253, 29, 29, 1), rgba(252, 176, 69, 1))`}
+        >
+          <Image
+            background={"#3a3636"}
+            src={displayPic}
+            borderRadius={"full"}
+            boxSize={"160px"}
+            margin={"auto"}
+          ></Image>
+        </Box>
+      </GridItem>
+      <GridItem
+        display={["block", "block", "none"]}
+        colSpan={[3, 3, 1, 1, 1, 1]}
+      >
+        <HStack justifyContent={["center", "center", "start"]}>
+          <FaGithub className="icons" size={32} />
+          <FaInstagram className="icons" size={32} />
+          <FaLinkedin className="icons" size={32} />
+        </HStack>
+      </GridItem>
+      <GridItem colSpan={3}>
+        <Heading
+          as={"h2"}
+          size={"2xl"}
+          className="mainIntro"
+          py={"0px"}
+          my={"12px"}
+        >
+          Hi I'm Malvin, a programmer with some ability to love learning and
+          working on teamwork.
+        </Heading>
+      </GridItem>
+      <GridItem
+        display={["none", "none", "inline"]}
+        colSpan={[3, 3, 1, 1, 1, 1]}
+      >
+        <Box
+          margin={"auto"}
+          display={"flex"}
+          borderRadius={"full"}
+          w={"170px"}
+          h={"170px"}
+          bgGradient={`linear(to-r, rgba(253, 29, 29, 1), rgba(252, 176, 69, 1))`}
+        >
+          <Image
+            background={"#3a3636"}
+            src={displayPic}
+            borderRadius={"full"}
+            boxSize={"160px"}
+            margin={"auto"}
+          ></Image>
+        </Box>
+      </GridItem>
+      <GridItem colSpan={[3, 3, 1, 1, 1, 1]}>
+        <Heading
+          as={"h3"}
+          size={"lg"}
+          textAlign={["center", "center", "start"]}
+        >
+          Biography
+        </Heading>
+        <Text color={"white"}>
           I'm Malvin, some call me Malv. I have a hobby of playing games,
           gadgets, and also like to talk about cars as well. I'm passionate
           about technologies and interested in Web Development, also interested
           about learning React.js
-        </p>
-      </div>
-      <div className="social">
-        <h3>Let's Connect</h3>
-        <div className="social-icons">
+        </Text>
+      </GridItem>
+      <GridItem
+        display={["none", "none", "block"]}
+        colSpan={[3, 3, 1, 1, 1, 1]}
+      >
+        <Heading
+          as={"h3"}
+          size={"lg"}
+          textAlign={["center", "center", "start"]}
+        >
+          Let's Connect
+        </Heading>
+        <HStack justifyContent={["center", "center", "start"]}>
           <FaGithub className="icons" size={32} />
           <FaInstagram className="icons" size={32} />
           <FaLinkedin className="icons" size={32} />
-        </div>
-      </div>
-      <div className="jobdesc">
-        <h3>What i do</h3>
-        <p style={{ textAlign: "center" }}>
+        </HStack>
+      </GridItem>
+      <GridItem colSpan={3}>
+        <Heading
+          mb={"48px"}
+          textAlign={"center"}
+          m={"auto"}
+          as={"h3"}
+          size={"lg"}
+        >
+          What i do
+        </Heading>
+      </GridItem>
+      <GridItem colSpan={3}>
+        <Text textAlign={"center"} color={"white"}>
           For now I still learning how to build and maintain websites, learnt a
           little bit of Frontend Web Development and still curious about Backend
           Development. So far, i've learnt HTML, CSS, Javascript. Currently
           learning about React.js.
-        </p>
-        <div className="languages-icons">
-          <img src={html5Logo} alt="" />
-          <img src={css3Logo} alt="" />
-          <img src={jsLogo} alt="" />
-          <img src={reactLogo} alt="" />
-        </div>
-      </div>
-    </div>
+        </Text>
+      </GridItem>
+      <GridItem colSpan={3}>
+        <HStack justifyContent={"center"}>
+          <Image src={html5Logo} boxSize={"100px"}></Image>
+          <Image src={css3Logo} boxSize={"100px"}></Image>
+          <Image src={jsLogo} boxSize={"100px"}></Image>
+          <Image src={reactLogo} boxSize={"100px"}></Image>
+        </HStack>
+      </GridItem>
+    </Grid>
   );
 }
 
@@ -87,21 +192,44 @@ function Card(props) {
 
 function Featured() {
   return (
-    <div className="featured-project">
+    <Box
+      margin={"0 auto"}
+      mb={"24px"}
+      maxW={["400px", "400px", "740px", "1110px"]}
+    >
       <h3>Featured Projects</h3>
-      <div className="featured-works">
-        <Card image={project1} title="The Band Landingpage" alt="" />
-        <Card image={project1} title="WIP" alt="" />
-        <Card image={project1} title="WIP" alt="" />
-      </div>
-    </div>
+      <Grid
+        gridTemplateColumns={"auto auto auto auto auto auto"}
+        gap={"12px"}
+        justifyItems={"center"}
+      >
+        <GridItem colSpan={[6, 6, 3, 2, 2, 2]}>
+          <Card image={project1} title="The Band Landing Page" alt="" />
+        </GridItem>
+        <GridItem colSpan={[6, 6, 3, 2, 2, 2]}>
+          <Card image={project1} title="WIP" alt="" />
+        </GridItem>
+        <GridItem colSpan={[6, 6, 3, 2, 2, 2]}>
+          <Card image={project1} title="WIP" alt="" />
+        </GridItem>
+        <GridItem colSpan={[6, 6, 3, 2, 2, 2]}>
+          <Card image={project1} title="WIP" alt="" />
+        </GridItem>
+        <GridItem colSpan={[6, 6, 3, 2, 2, 2]}>
+          <Card image={project1} title="WIP" alt="" />
+        </GridItem>
+        <GridItem colSpan={[6, 6, 3, 2, 2, 2]}>
+          <Card image={project1} title="WIP" alt="" />
+        </GridItem>
+      </Grid>
+    </Box>
   );
 }
 
 function Footer() {
   return (
     <div className="footer">
-      <hr />
+      <Divider height={"1px"} maxWidth={"1140px"}></Divider>
       <p>Reach me out</p>
       <div className="footer-social-icons">
         <FaGithub className="icons" size={32} />
@@ -116,32 +244,42 @@ function Footer() {
 
 function Homepage() {
   return (
-    <div className="container">
-      <Intro intro="Hi I'm Malvin, a programmer with some ability to love learning and working on teamwork." />
+    <>
+      <Intro />
       <Featured />
-    </div>
+    </>
   );
 }
 
 function Portfolio() {
   return (
-    <div className="container">
+    <Box mx={"auto"} mb={"24px"} maxW={["400px", "400px", "740px", "1110px"]}>
       <h2>All Projects</h2>
-      <div className="featured-works">
-        <Card image={project1} title="The Band Landingpage" alt="" />
-        <Card image={project1} title="WIP" alt="" />
-        <Card image={project1} title="WIP" alt="" />
-        <Card image={project1} title="WIP" alt="" />
-        <Card image={project1} title="WIP" alt="" />
-        <Card image={project1} title="WIP" alt="" />
-        <Card image={project1} title="WIP" alt="" />
-        <Card image={project1} title="WIP" alt="" />
-        <Card image={project1} title="WIP" alt="" />
-        <Card image={project1} title="WIP" alt="" />
-        <Card image={project1} title="WIP" alt="" />
-        <Card image={project1} title="WIP" alt="" />
-      </div>
-    </div>
+      <Grid
+        gridTemplateColumns={"auto auto auto auto auto auto"}
+        gap={"12px"}
+        justifyItems={"center"}
+      >
+        <GridItem colSpan={[6, 6, 3, 2, 2, 2]}>
+          <Card image={project1} title="The Band Landing Page" alt="" />
+        </GridItem>
+        <GridItem colSpan={[6, 6, 3, 2, 2, 2]}>
+          <Card image={project1} title="WIP" alt="" />
+        </GridItem>
+        <GridItem colSpan={[6, 6, 3, 2, 2, 2]}>
+          <Card image={project1} title="WIP" alt="" />
+        </GridItem>
+        <GridItem colSpan={[6, 6, 3, 2, 2, 2]}>
+          <Card image={project1} title="WIP" alt="" />
+        </GridItem>
+        <GridItem colSpan={[6, 6, 3, 2, 2, 2]}>
+          <Card image={project1} title="WIP" alt="" />
+        </GridItem>
+        <GridItem colSpan={[6, 6, 3, 2, 2, 2]}>
+          <Card image={project1} title="WIP" alt="" />
+        </GridItem>
+      </Grid>
+    </Box>
   );
 }
 
@@ -149,11 +287,13 @@ function App() {
   return (
     <>
       <Navbar name="Malvin A." />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-      </Routes>
-      <Footer />
+      <Container maxW={"1140px"}>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+        </Routes>
+        <Footer />
+      </Container>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,35 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: () => ({
+      body: {
+        bg: "",
+      },
+    }),
+  },
+  fonts: {
+    heading: `'Sora', sans-serif`,
+    body: `'Inter', sans-serif`,
+  },
+  base: "0px",
+  sm: "320px",
+  md: "768px",
+  lg: "960px",
+  xl: "1200px",
+  "2xl": "1536px",
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
